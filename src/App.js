996551de22f19973346efe77397d406f0a9f06d5
Ashmit Yadav <beingashmit@gmail.com>
2023-04-27 04:49:49 +0530

    setup completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 function App() {
   return (
     <div className="App">
-     <h1>React Weather App</h1>
+     <h1 className='text-200 font-bold text-red-500'>React Weather App</h1>
+     <UilReact size={50}/>
     </div>
   );
 }
